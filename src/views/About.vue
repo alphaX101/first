b569cbf3f86a8a761e63857cbe3f79bbed4489a6
fangao <gaofan10101@gmail.com>
2021-02-05 18:01:55 +0800

    My first commit

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div class="about">
+    <img
+      src="../assets/risunok-art-dzhoker-art-joker-joaquin-phoenix-khoakin-feniks.jpg"
+      alt=""
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style  scoped>
+img {
+  width: 100%;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/lf.jpg" />
+    <HelloWorld msg="Welcome to my App index" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+img
+  width: 300px
+</style>
